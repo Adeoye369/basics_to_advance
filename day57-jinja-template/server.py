64026f4f4e5ffb67ec05a9fb75399b951bdce428
Adegbite Adeoye <adegbite.adeoye0@gmail.com>
2024-11-11 06:08:47 +0100

    using flask with api

--- day57-jinja-template/server.py
+++ day57-jinja-template/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
-import random, datetime as dt
+import random, requests, json, datetime as dt
+
 
 app = Flask(__name__)
 
@@ -8,6 +9,32 @@ def index_def():
     val = random.randint(1, 33)
     current_year = dt.datetime.now().year
     return render_template("index.html", val = val, year= current_year)
+
+
+@app.route('/guess/<name>')
+def guess_page(name):
+
+    result = ""
+    gender = age = prob = None
+    try :
+        # Age data from agify
+        res = requests.get(f"https://api.agify.io?name={name}")
+        result = json.loads(res.text)
+        age = result['age']
+
+        # Gender data from genderize
+        res = requests.get(f"https://api.genderize.io?name={name}")
+        result = json.loads(res.text)
+        gender = result["gender"]
+        prob = round(result["probability"]*100)
+        
+    except KeyError as e:
+        print(e)
+        print(result)
+
+
+    return render_template("guess.html", name = name.title(), age=age, gender = gender, prob = prob, result= result)
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
