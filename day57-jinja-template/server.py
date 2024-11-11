c9f364e4859f88eba53607ffd565ae6ffb601ce4
Adegbite Adeoye <adegbite.adeoye0@gmail.com>
2024-11-11 04:02:13 +0100

    auto update year on website

--- day57-jinja-template/server.py
+++ day57-jinja-template/server.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-import random
+import random, datetime as dt
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_def():
     val = random.randint(1, 33)
-    return render_template("index.html", val = val)
+    current_year = dt.datetime.now().year
+    return render_template("index.html", val = val, year= current_year)
 
 if __name__ == "__main__" :
     app.run(debug=True)
